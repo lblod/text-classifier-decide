37c59676e2252ccb91e20ee639fc5ddfb3dede7f
Joachim van der Herten <joachim@ml2grow.com>
2025-09-03 15:35:17 +0200

    feat: add multilabel classification

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import json
+from abc import abstractmethod, ABC
+
 import numpy as np
-from functools import partial
 
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from transformers import AutoTokenizer, DataCollatorWithPadding, AutoModelForSequenceClassification, TrainingArguments, Trainer, pipeline
@@ -9,17 +10,25 @@ from ld import write_airo_ai_model
 import fire
 
 
-class Metrics:
+def sigmoid(x):
+   return 1/(1 + np.exp(-x))
+
+
+class Metrics(ABC):
+
+    @abstractmethod
+    def map_predictions(self, predictions):
+        pass
 
     def compute(self, eval_pred):
         preds, labels = eval_pred
-        preds = np.argmax(preds, axis=1)
-        accuracy = accuracy_score(labels, preds)
+        preds = self.map_predictions(preds)
+        accuracy = accuracy_score(labels.astype(int), preds)
 
         # Calculate precision, recall, and F1-score
-        precision = precision_score(labels, preds, average='weighted')
-        recall = recall_score(labels, preds, average='weighted')
-        f1 = f1_score(labels, preds, average='weighted')
+        precision = precision_score(labels.astype(int), preds, average='weighted')
+        recall = recall_score(labels.astype(int), preds, average='weighted')
+        f1 = f1_score(labels.astype(int), preds, average='weighted')
 
         return {
             'accuracy': accuracy,
@@ -27,6 +36,17 @@ class Metrics:
             'recall': recall,
             'f1': f1
         }
+
+
+class SingleLabelMetrics(Metrics):
+    def map_predictions(self, predictions):
+        return np.argmax(predictions, axis=1).astype(int)
+
+
+class MultiLabelMetrics(Metrics):
+    def map_predictions(self, predictions):
+        predictions = sigmoid(predictions)
+        return (predictions > 0.5).astype(int)
 
 
 def read_data(file_path: str):
@@ -38,9 +58,25 @@ def read_data(file_path: str):
     return data
 
 
-def format_data(data, label2id) -> Dataset:
+def format_data_single(data, label2id) -> Dataset:
     return Dataset.from_list([
-        {"text": task["data"]["text"], "label": label2id[task["annotations"][0]["result"][0]["value"]["choices"][0]]}
+        {
+            "text": task["data"]["text"],
+            "label": label2id[task["annotations"][0]["result"][0]["value"]["choices"][0]]
+        }
+        for task in data if task["annotations"][0]["result"]
+    ])
+
+
+def format_data_multi(data, label2id) -> Dataset:
+    return Dataset.from_list([
+        {
+            "text": task["data"]["text"],
+            "label": np.isin(
+                np.arange(len(label2id)),
+                [label2id[l] for l in task["annotations"][0]["result"][0]["value"]["choices"]]
+            ).astype(float).tolist()
+        }
         for task in data if task["annotations"][0]["result"]
     ])
 
@@ -52,36 +88,56 @@ def generate_label_map(data):
     return label2id, id2label
 
 
-def train(file_path: str, model_id: str):
+def train(
+        file_path: str,
+        model_id: str,
+        transformer: str = "distilbert/distilbert-base-uncased",
+        learning_rate: float = 2e-5,
+        epochs: int = 2,
+        weight_decay: float = 0.01,
+        problem_type: str = "single_label_classification"
+):
+    option_map = {
+        'single_label_classification': (SingleLabelMetrics, format_data_single),
+        'multi_label_classification': (MultiLabelMetrics, format_data_multi)
+    }
+
+    metrics, format_data = option_map[problem_type]
     data = read_data(file_path)
     label2id, id2label = generate_label_map(data)
     dataset = format_data(data, label2id)
 
-    tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
-    model = AutoModelForSequenceClassification.from_pretrained(
-        "distilbert/distilbert-base-uncased",
-        num_labels=len(id2label),
-        id2label=id2label,
-        label2id=label2id
-    )
-    metrics = Metrics()
-    dataset = dataset.class_encode_column("label")
-    dataset = dataset.train_test_split(test_size=0.1, stratify_by_column="label")
+    tokenizer = AutoTokenizer.from_pretrained(transformer)
+
+    if problem_type == 'single_label_classification':
+        dataset = dataset.class_encode_column("label")
+        dataset = dataset.train_test_split(test_size=0.1, stratify_by_column="label")
+    else:
+        dataset = dataset.train_test_split(test_size=0.1)
+
     tokenized_data = dataset.map(lambda examples: tokenizer(examples["text"], truncation=True), batched=True)
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
     training_args = TrainingArguments(
         output_dir="uc1-uc2-model",
-        learning_rate=2e-5,
+        learning_rate=learning_rate,
         per_device_train_batch_size=32,
         per_device_eval_batch_size=32,
-        num_train_epochs=2,
-        weight_decay=0.01,
+        num_train_epochs=epochs,
+        weight_decay=weight_decay,
         eval_strategy="epoch",
         save_strategy="epoch",
         load_best_model_at_end=True,
         push_to_hub=False,
         push_to_hub_model_id=model_id
+    )
+
+    model = AutoModelForSequenceClassification.from_pretrained(
+        transformer,
+        num_labels=len(id2label),
+        id2label=id2label,
+        label2id=label2id,
+        problem_type=problem_type
     )
 
     trainer = Trainer(
@@ -91,7 +147,7 @@ def train(file_path: str, model_id: str):
         eval_dataset=tokenized_data["test"],
         processing_class=tokenizer,
         data_collator=data_collator,
-        compute_metrics=metrics.compute,
+        compute_metrics=metrics().compute,
     )
 
 
